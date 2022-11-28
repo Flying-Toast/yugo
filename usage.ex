0fa8b74656f1731aa174d80d8198dc5f8c7d54af
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2022-11-28 12:12:49 -0500

    model out usage

---
+++ usage.ex
@@ -0,0 +1,28 @@
+defmodule Application do
+  def start() do
+    children = [
+      {Getmail.IMAPClient, name: :my_client, username: "foo@example.com", password: "pa55w0rd", server: "imap.example.com"}
+    ]
+  end
+end
+
+defmodule MailHandler do
+  use GenServer
+
+  def init() do
+    my_filter = Getmail.Filter.all() # allows all email thru
+                |> Getmail.Filter.mailbox("INBOX") # filters down to only messages in inbox
+                |> Getmail.Filter.has_flag(:unseen) # only unseen messages
+                |> Getmail.Filter.select(:subject) # only selects the subject field (not body, attachments, etc)
+
+    Getmail.subscribe(:my_client, my_filter)
+  end
+
+  def handle_info({:email, client, message}, state) do
+    Getmail.set_flag(client, message, :seen)
+
+    IO.puts("Received an email: #{message.subject}")
+
+    {:noreply, state}
+  end
+end
