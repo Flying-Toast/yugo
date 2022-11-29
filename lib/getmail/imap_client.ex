fd47e87fb6bb760fb01f62ef00a212c2c4a3f80e
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2022-11-28 20:35:52 -0500

    handle synchronizing literals from the server

--- lib/getmail/imap_client.ex
+++ lib/getmail/imap_client.ex
@@ -15,14 +15,15 @@ defmodule Getmail.IMAPClient do
       opts
       |> Keyword.put_new(:port, 993)
       |> Keyword.put_new(:tls, true)
+      |> Keyword.update!(:server, &to_charlist/1)
 
     name = {:via, Registry, {Getmail.Registry, opts[:name]}}
-    GenServer.start_link(__MODULE__, init_arg, name)
+    GenServer.start_link(__MODULE__, init_arg, name: name)
   end
 
   @impl true
   def init(args) do
-    common_connect_opts = [packet: :line, active: :once]
+    common_connect_opts = [packet: :line, active: :once, mode: :binary]
 
     {:ok, socket} =
       if args[:tls] do
@@ -52,19 +53,49 @@ defmodule Getmail.IMAPClient do
 
   @impl true
   def handle_info({socket_kind, socket, data}, conn) when socket_kind in [:ssl, :tcp] do
-    #case parse(data) do
-    #  {:error, :expected_more_data} ->
-    #    ==LOOP==
-    #    data.append(:gen_tcp.recv(socket, 0))
-    #    if {:error, :expected_more_data} = parse(data), do: goto ==LOOP==
-    #end
-    IO.puts(data) ###############
+    data =
+      # detect a synchonizing literal and parse the required number of bytes
+      case Regex.run(~r/\{(\d+)\}\r\n$/, data, capture: :all_but_first) do
+        [n] ->
+          n = String.to_integer(n) + 2 # add 2 to account for the final \r\n
+          packet_lines = [data | recv_n_bytes(conn, n)]
+          Enum.join(packet_lines)
 
-    # we set [active: :once] each time so that we can parse packets that might have [non-]synchronizing literals (see above)
-    :ok = :inet.setopts(socket, active: :once)
+        _ ->
+          data
+      end
 
-    #conn = handle_packet(parse(data), conn)
+    # we set [active: :once] each time so that we can parse packets that have synchronizing literals (see above)
+    :ok =
+      if conn.tls do
+        :ssl.setopts(socket, active: :once)
+      else
+        :inet.setopts(socket, active: :once)
+      end
+
+    conn = handle_packet(data, conn)
 
     {:noreply, conn}
+  end
+
+  defp recv_n_bytes(%Conn{} = conn, n, acc \\ []) do
+    if n > 0 do
+      {:ok, next_line} =
+        if conn.tls do
+          :ssl.recv(conn.socket, 0)
+        else
+          :gen_tcp.recv(conn.socket, 0)
+        end
+
+      recv_n_bytes(conn, n - String.length(next_line), [next_line | acc])
+    else
+      Enum.reverse(acc)
+    end
+  end
+
+  defp handle_packet(data, conn) do
+    IO.puts("GOT PACKET: ...#{data}...")
+
+    conn
   end
 end
