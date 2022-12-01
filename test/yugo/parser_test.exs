01f77a62167c5225831b18462a6681950d2b5612
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2022-11-30 20:21:47 -0500

    tests

--- test/yugo/parser_test.exs
+++ test/yugo/parser_test.exs
@@ -33,11 +33,25 @@ defmodule Yugo.ParserTest do
     [:continuation] = Parser.parse_response("+ \r\n")
   end
 
-  test "parse PERMANENTFLAGS" do
+  test "parse untagged SELECT responses" do
     [permanent_flags: ["\\DELETED", "\\SEEN", "\\*"]] =
       Parser.parse_response("* OK [PERMANENTFLAGS (\\Deleted \\Seen \\*)] Limited\r\n")
 
     [permanent_flags: []] =
       Parser.parse_response("* OK [PERMANENTFLAGS ()] No permanent flags permitted\r\n")
+
+    [first_unseen: 12] = Parser.parse_response("* OK [UNSEEN 12] Message 12 is first unseen\r\n")
+
+    [uid_validity: 3_857_529_045] =
+      Parser.parse_response("* OK [UIDVALIDITY 3857529045] UIDs valid\r\n")
+
+    [uid_next: 4392] =
+      Parser.parse_response("* OK [UIDNEXT 4392] Predicted next UID\r\n")
+
+    [applicable_flags: ["\\ANSWERED", "\\FLAGGED", "\\DELETED", "\\SEEN", "\\DRAFT"]] =
+      Parser.parse_response("* FLAGS (\\Answered \\Flagged \\Deleted \\Seen \\Draft)\r\n")
+
+    [applicable_flags: []] =
+      Parser.parse_response("* FLAGS () nope no flags\r\n")
   end
 end
