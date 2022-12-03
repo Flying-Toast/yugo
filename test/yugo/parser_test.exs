fb572653a21234b107ae3bcce5278ff90d876c44
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2022-12-02 23:43:03 -0500

    add a test

--- test/yugo/parser_test.exs
+++ test/yugo/parser_test.exs
@@ -53,16 +53,17 @@ defmodule Yugo.ParserTest do
     [applicable_flags: []] = Parser.parse_response("* FLAGS () nope no flags\r\n")
   end
 
-  test "parse FETCH envelopes" do
+  test "parse FETCH responses" do
+    [
+      fetch: {123, :flags, []}
+    ] = Parser.parse_response(~S|* 123 fetch (flags ())|)
+
     [
       fetch:
-        {123, :envelope,
+        {12, :envelope,
          %{
            bcc: [],
-           cc: [
-             "minutes@cnri.reston.va.us",
-             "klensin@mit.edu"
-           ],
+           cc: ["minutes@cnri.reston.va.us", "klensin@mit.edu"],
            date: ~U[1996-07-16 19:23:25Z],
            from: ["gray@cac.washington.edu"],
            in_reply_to: nil,
@@ -71,10 +72,11 @@ defmodule Yugo.ParserTest do
            sender: ["gray@cac.washington.edu"],
            subject: "IMAP4rev1 WG mtg summary and minutes",
            to: ["imap@cac.washington.edu"]
-         }}
+         }},
+      fetch: {12, :flags, ["\\Seen"]}
     ] =
       Parser.parse_response(
-        ~S|* 123 FETCH (ENVELOPE ("Wed, 17 Jul 1996 02:23:25 -0700 (PDT)" "IMAP4rev1 WG mtg summary and minutes" (("Terry Gray" NIL "gray" "cac.washington.edu")) (("Terry Gray" NIL "gray" "cac.washington.edu")) (("Terry Gray" NIL "gray" "cac.washington.edu")) ((NIL NIL "imap" "cac.washington.edu")) ((NIL NIL "minutes" "CNRI.Reston.VA.US")("John Klensin" NIL "KLENSIN" "MIT.EDU")) NIL NIL "<B27397-0100000@cac.washington.edu>"))|
+        ~S|* 12 FETCH (FLAGS (\Seen) ENVELOPE ("Wed, 17 Jul 1996 02:23:25 -0700 (PDT)" "IMAP4rev1 WG mtg summary and minutes" (("Terry Gray" NIL "gray" "cac.washington.edu")) (("Terry Gray" NIL "gray" "cac.washington.edu")) (("Terry Gray" NIL "gray" "cac.washington.edu")) ((NIL NIL "imap" "cac.washington.edu")) ((NIL NIL "minutes" "CNRI.Reston.VA.US")("John Klensin" NIL "KLENSIN" "MIT.EDU")) NIL NIL "<B27397-0100000@cac.washington.edu>"))|
       )
   end
 end
