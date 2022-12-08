5fb43386695a4b39f205404edc74251e4b6838b8
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2022-12-08 18:34:20 -0500

    test filter matching

--- test/yugo/filter_test.exs
+++ test/yugo/filter_test.exs
@@ -36,4 +36,44 @@ defmodule Yugo.FilterTest do
       |> Filter.lacks_flag(:draft)
       |> Filter.lacks_flag(:draft)
   end
+
+  test "filters on subject_matches" do
+    f =
+      Filter.all()
+      |> Filter.subject_matches(~r/(hello|goodbye)/)
+
+    assert not Filter.accepts?(f, %{envelope: %{subject: nil}})
+    assert not Filter.accepts?(f, %{envelope: %{subject: ""}})
+    assert Filter.accepts?(f, %{envelope: %{subject: "this subject has 'hello' in it"}})
+    assert Filter.accepts?(f, %{envelope: %{subject: "goodgoodbye"}})
+  end
+
+  test "filters on flags" do
+    has_seen_flag =
+      Filter.all()
+      |> Filter.has_flag(:seen)
+
+    assert not Filter.accepts?(has_seen_flag, %{flags: []})
+    assert Filter.accepts?(has_seen_flag, %{flags: [:seen]})
+    assert Filter.accepts?(has_seen_flag, %{flags: [:recent, :seen, :draft]})
+
+    has_seen_lacks_draft =
+      has_seen_flag
+      |> Filter.lacks_flag(:draft)
+
+    assert not Filter.accepts?(has_seen_lacks_draft, %{flags: [:draft]})
+    assert not Filter.accepts?(has_seen_lacks_draft, %{flags: [:seen, :draft]})
+    assert Filter.accepts?(has_seen_lacks_draft, %{flags: [:seen, :deleted]})
+  end
+
+  test "filters on sender" do
+    f =
+      Filter.all()
+      |> Filter.sender_matches(~r/bob/)
+
+    assert not Filter.accepts?(f, %{envelope: %{sender: []}})
+    assert not Filter.accepts?(f, %{envelope: %{sender: ["foo@bar.com", "baz@biz.com"]}})
+    assert Filter.accepts?(f, %{envelope: %{sender: ["foo@bar.com", "bob@example.com"]}})
+    assert Filter.accepts?(f, %{envelope: %{sender: ["zzz@foo.bob"]}})
+  end
 end
